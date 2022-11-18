307692b0991238acbc96ac9f3cf5d297ffecc769
Blake Anderson <blakeanderson@utexas.edu>
2022-11-18 15:17:12 -0600

    Rename description.launch.py to state_publisher.launch.py

--- spot_description/launch/state_publisher.launch.py
+++ spot_description/launch/state_publisher.launch.py
@@ -1,21 +1,22 @@
 import os
 
 import launch
+from launch.actions import DeclareLaunchArgument
 from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
 
-import launch_ros.actions
+from launch_ros.actions import Node
 from launch_ros.descriptions import ParameterValue
 from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
 
     launch_args = [
-        launch.actions.DeclareLaunchArgument('has_arm',
+        DeclareLaunchArgument('has_arm',
             description='Boolean. Include the Spot Arm.',
             choices=['True', 'False'],
             default_value='False'),
             
-        launch.actions.DeclareLaunchArgument('has_eap',
+        DeclareLaunchArgument('has_eap',
             description='Boolean. Include the Enhanced Autonomy package (EAP)',
             choices=['True', 'False'],
             default_value='False')
@@ -23,6 +24,7 @@ def generate_launch_description():
 
     has_arm = LaunchConfiguration('has_arm')
     has_eap = LaunchConfiguration('has_eap')
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
     this_pkg_share = FindPackageShare('spot_description')
     
     # Build the URDF from the xacro, applying specified hardware accessories.
@@ -31,15 +33,20 @@ def generate_launch_description():
         Command(['xacro ', xacro_path, ' has_arm:=',has_arm, ' has_eap:=',has_eap]),
         value_type=str)
 
-    rsp = launch_ros.actions.Node(package='robot_state_publisher',
-                                  executable='robot_state_publisher',
-                                  output='both',
-                                  parameters=[{
-                                    'robot_description': urdf_param
-                                  }]
-                                 )
-
     return launch.LaunchDescription([
         *launch_args,
-        rsp
+
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='False',
+            description='Use simulation clock if True'),
+
+        Node(package='robot_state_publisher',
+               executable='robot_state_publisher',
+               output='both',
+               parameters=[{
+                    'robot_description': urdf_param,
+                    'use_sim_time': use_sim_time
+               }]
+            )
     ])
