0d6c2bbeb3f8a049e3249d546e1a125b037f4a3e
alexnavtt <alexnavtt@utexas.edu>
2024-07-25 09:09:38 -0600

    Added a new behavior to record a frame's current pose for later use

---
+++ spot_behaviors/include/spot_behaviors/record_current_location.hpp
@@ -0,0 +1,61 @@
+////////////////////////////////////////////////////////////////////////////////////////////
+//      Title     : record_current_location.hpp
+//      Project   : spot_ros
+//      Copyright : Copyright© The University of Texas at Austin, 2024. All rights reserved.
+//                
+//          All files within this directory are subject to the following, unless an alternative
+//          license is explicitly included within the text of each file.
+//
+//          This software and documentation constitute an unpublished work
+//          and contain valuable trade secrets and proprietary information
+//          belonging to the University. None of the foregoing material may be
+//          copied or duplicated or disclosed without the express, written
+//          permission of the University. THE UNIVERSITY EXPRESSLY DISCLAIMS ANY
+//          AND ALL WARRANTIES CONCERNING THIS SOFTWARE AND DOCUMENTATION,
+//          INCLUDING ANY WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+//          PARTICULAR PURPOSE, AND WARRANTIES OF PERFORMANCE, AND ANY WARRANTY
+//          THAT MIGHT OTHERWISE ARISE FROM COURSE OF DEALING OR USAGE OF TRADE.
+//          NO WARRANTY IS EITHER EXPRESS OR IMPLIED WITH RESPECT TO THE USE OF
+//          THE SOFTWARE OR DOCUMENTATION. Under no circumstances shall the
+//          University be liable for incidental, special, indirect, direct or
+//          consequential damages or loss of profits, interruption of business,
+//          or related expenses which may arise from use of software or documentation,
+//          including but not limited to those resulting from defects in software
+//          and/or documentation, or loss or inaccuracy of data of any kind.
+//
+////////////////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+#include <thread>
+#include <optional>
+#include <rclcpp/rclcpp.hpp>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
+#include <behaviortree_cpp/action_node.h>
+
+#include "spot_msgs/msg/battery_state_array.hpp"
+
+namespace spot_behaviors {
+
+class RecordCurrentLocation : public BT::SyncActionNode {
+public:
+    RecordCurrentLocation(const std::string& name, const BT::NodeConfiguration& config);
+
+    static BT::PortsList providedPorts();
+
+    // Returns SUCCESS if battery is over a given threshold, FAILURE otherwise
+    BT::NodeStatus tick() override;
+
+private:
+    // The node instance to use to collect data
+    rclcpp::Node::SharedPtr node_;
+
+    // TF listener to get robot pose
+    tf2_ros::Buffer tf_buffer_;
+    tf2_ros::TransformListener tf_listener_;
+
+    // How many instances of such BT nodes have been created (used to avoid namespace conflicts)
+    static inline int node_count_ = 0;
+};
+
+} // namespace spot_behaviors
---
+++ spot_behaviors/src/record_current_location.cpp
@@ -0,0 +1,48 @@
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include "spot_behaviors/record_current_location.hpp"
+
+namespace spot_behaviors{
+    
+RecordCurrentLocation::RecordCurrentLocation(const std::string& name, const BT::NodeConfiguration& config) :
+    BT::SyncActionNode(name, config),
+    node_(std::make_shared<rclcpp::Node>(name+"BT"+std::to_string(node_count_++), "spot_behaviors")),
+    tf_buffer_(node_->get_clock()),
+    tf_listener_(tf_buffer_)
+{}
+
+BT::PortsList RecordCurrentLocation::providedPorts() {
+    return {
+        BT::InputPort<std::string>("global_frame", "The frame in which to locate the robot. Defaults to 'map'"),
+        BT::InputPort<std::string>("robot_frame", "The frame whose location to record. Defaults to 'base_link'"),
+        BT::OutputPort<geometry_msgs::msg::PoseStamped>("recorded_pose", "The measured pose")
+    };
+}
+
+BT::NodeStatus RecordCurrentLocation::tick() {
+    const std::string global_frame = getInput<std::string>("global_frame").value_or("map");
+    const std::string query_frame = getInput<std::string>("robot_frame").value_or("base_link");
+
+    if (std::string err; !tf_buffer_.canTransform(global_frame, query_frame, tf2::TimePointZero, std::chrono::seconds(2), &err)) {
+        RCLCPP_ERROR(node_->get_logger(), "Unable to record frame location: %s", err.c_str());
+        return BT::NodeStatus::FAILURE;
+    }
+
+    const geometry_msgs::msg::TransformStamped frame_transform = tf_buffer_.lookupTransform(
+        global_frame,
+        query_frame,
+        tf2::TimePointZero
+    );
+
+    geometry_msgs::msg::PoseStamped frame_pose;
+    frame_pose.header.stamp = node_->now();
+    frame_pose.header.frame_id = global_frame;
+    frame_pose.pose.position.x = frame_transform.transform.translation.x;
+    frame_pose.pose.position.y = frame_transform.transform.translation.y;
+    frame_pose.pose.position.z = frame_transform.transform.translation.z;
+    frame_pose.pose.orientation = frame_transform.transform.rotation;
+
+    setOutput<geometry_msgs::msg::PoseStamped>("recorded_pose", frame_pose);
+    return BT::NodeStatus::SUCCESS;
+}
+
+} // namespace spot_behaviors
