28eb0686b9dec6fca09bccf86681332cca23db81
alexnavtt <alexnavtt@utexas.edu>
2024-08-14 18:05:17 -0600

    Added a behavior to check if the arm is in collision

---
+++ spot_behaviors/src/check_hand_collision.cpp
@@ -0,0 +1,65 @@
+////////////////////////////////////////////////////////////////////////////////////////////
+//      Title     : check_hand_collision.cpp
+//      Project   : spot_ros
+//      Copyright : Copyright© The University of Texas at Austin, 2024. All rights reserved.
+//                
+//          All files within this directory are subject to the following, unless an alternative
+//          license is explicitly included within the text of each file.
+//
+//          This software and documentation constitute an unpublished work
+//          and contain valuable trade secrets and proprietary information
+//          belonging to the University. None of the foregoing material may be
+//          copied or duplicated or disclosed without the express, written
+//          permission of the University. THE UNIVERSITY EXPRESSLY DISCLAIMS ANY
+//          AND ALL WARRANTIES CONCERNING THIS SOFTWARE AND DOCUMENTATION,
+//          INCLUDING ANY WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+//          PARTICULAR PURPOSE, AND WARRANTIES OF PERFORMANCE, AND ANY WARRANTY
+//          THAT MIGHT OTHERWISE ARISE FROM COURSE OF DEALING OR USAGE OF TRADE.
+//          NO WARRANTY IS EITHER EXPRESS OR IMPLIED WITH RESPECT TO THE USE OF
+//          THE SOFTWARE OR DOCUMENTATION. Under no circumstances shall the
+//          University be liable for incidental, special, indirect, direct or
+//          consequential damages or loss of profits, interruption of business,
+//          or related expenses which may arise from use of software or documentation,
+//          including but not limited to those resulting from defects in software
+//          and/or documentation, or loss or inaccuracy of data of any kind.
+//
+////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "spot_behaviors/check_hand_collision.hpp"
+
+namespace spot_behaviors {
+
+CheckHandCollision::CheckHandCollision(const std::string& name, const BT::NodeConfiguration& config, tf2_ros::Buffer::SharedPtr tf_buffer) :
+    BT::SyncActionNode(name, config),
+    NodeBehaviorBase(name, tf_buffer)
+    {
+        manipulator_sub_ = this->create_subscription<std_msgs::msg::Bool>(
+            "/spot_manipulation_driver/manipulator_state/is_hand_in_collision",
+            rclcpp::ParametersQoS{},
+            std::bind(&CheckHandCollision::collisionStateCallback, this, std::placeholders::_1)
+        );
+        spin_thread_ = std::thread([this](){rclcpp::spin(this->get_node_base_interface());});
+    }
+
+BT::PortsList CheckHandCollision::providedPorts() {
+    return {};
+}
+
+BT::NodeStatus CheckHandCollision::tick() {
+    if (!in_collision_.has_value()){
+        RCLCPP_ERROR(get_logger(), "No messages received on topic %s", manipulator_sub_->get_topic_name());
+        return BT::NodeStatus::FAILURE;
+    }
+
+    if (in_collision_.value()) {
+        RCLCPP_WARN(get_logger(), "Hand is in collision");
+        return BT::NodeStatus::FAILURE;
+    }
+    return BT::NodeStatus::SUCCESS;
+}
+
+void CheckHandCollision::collisionStateCallback(std_msgs::msg::Bool::UniquePtr msg){
+    in_collision_ = msg->data;
+}
+
+} // namespace spot_behaviors
