2a4ab56deb275de9be9f7a1e5662a1d57f0ffa30
Crasun Jans <jpanthi@utexas.edu>
2025-01-16 17:10:39 -0600

    update output port to use shared_ptr

--- spot_behaviors/src/record_current_location.cpp
+++ spot_behaviors/src/record_current_location.cpp
@@ -12,7 +12,7 @@ BT::PortsList RecordCurrentLocation::providedPorts() {
     return {
         BT::InputPort<std::string>("global_frame", "The frame in which to locate the robot. Defaults to 'map'"),
         BT::InputPort<std::string>("robot_frame", "The frame whose location to record. Defaults to 'base_link'"),
-        BT::OutputPort<geometry_msgs::msg::PoseStamped>("recorded_pose", "The measured pose")
+        BT::OutputPort<std::shared_ptr<geometry_msgs::msg::PoseStamped>>("recorded_pose", "The measured pose")
     };
 }
 
@@ -31,15 +31,15 @@ BT::NodeStatus RecordCurrentLocation::tick() {
         tf2::TimePointZero
     );
 
-    geometry_msgs::msg::PoseStamped frame_pose;
-    frame_pose.header.stamp = rclcpp::Time(0);
-    frame_pose.header.frame_id = global_frame;
-    frame_pose.pose.position.x = frame_transform.transform.translation.x;
-    frame_pose.pose.position.y = frame_transform.transform.translation.y;
-    frame_pose.pose.position.z = frame_transform.transform.translation.z;
-    frame_pose.pose.orientation = frame_transform.transform.rotation;
+    std::shared_ptr<geometry_msgs::msg::PoseStamped> frame_pose;
+    frame_pose->header.stamp = rclcpp::Time(0);
+    frame_pose->header.frame_id = global_frame;
+    frame_pose->pose.position.x = frame_transform.transform.translation.x;
+    frame_pose->pose.position.y = frame_transform.transform.translation.y;
+    frame_pose->pose.position.z = frame_transform.transform.translation.z;
+    frame_pose->pose.orientation = frame_transform.transform.rotation;
 
-    setOutput<geometry_msgs::msg::PoseStamped>("recorded_pose", frame_pose);
+    setOutput<std::shared_ptr<geometry_msgs::msg::PoseStamped>>("recorded_pose", frame_pose);
     return BT::NodeStatus::SUCCESS;
 }
 
