69cb2513d35981661f1674bca1023b9ab8413c7e
alexnavtt <alexnavtt@utexas.edu>
2025-03-18 14:35:54 -0600

    Edited names to indicate that slam starts where the robot starts

--- spot_navigation/scripts/publish_dock_for_slam.py
+++ spot_navigation/scripts/publish_dock_for_slam.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import rclpy
 import rclpy.duration
 from rclpy.node import Node
@@ -8,28 +8,28 @@ from tf2_ros import Buffer, TransformListener, StaticTransformBroadcaster
 def main():
     rclpy.init()
 
-    node = Node("slam_dock_frame_publisher")
+    node = Node("slam_inital_pose_frame_publisher")
     tf_buffer = Buffer()
     tf_listener = TransformListener(tf_buffer, node)
     tf_broadcaster = StaticTransformBroadcaster(node)
 
-    dock_frame = 'base_footprint'
+    intial_pose_frame = 'base_footprint'
     odom_frame = 'odom'
 
     def publish_transform():
         nonlocal timer
         try:
-            transform = tf_buffer.lookup_transform(dock_frame, odom_frame, rclpy.time.Time(), rclpy.duration.Duration(seconds=5.0))
+            transform = tf_buffer.lookup_transform(intial_pose_frame, odom_frame, rclpy.time.Time(), rclpy.duration.Duration(seconds=5.0))
         except Exception as e:
-            node.get_logger().error(f'Unable to publish dock transform: {e}')
+            node.get_logger().error(f'Unable to publish "slam_initial_pose" transform: {e}')
             rclpy.shutdown()
             exit(0)
 
-        transform.header.frame_id = 'dock'
+        transform.header.frame_id = 'slam_initial_pose'
         transform.child_frame_id = odom_frame
         tf_broadcaster.sendTransform(transform)
 
-        node.get_logger().info('Dock transform published')
+        node.get_logger().info('Initial transform published')
         timer.destroy()
 
     timer = node.create_timer(0.5, publish_transform)
