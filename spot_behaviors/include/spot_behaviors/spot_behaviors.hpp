c03860449e9a56dc05b446cc6387da6435b208bd
alexnavtt <alexnavtt@utexas.edu>
2025-03-18 14:29:33 -0600

    Removed NavigateToPose, which now lives in nrg_behaviors

--- spot_behaviors/include/spot_behaviors/spot_behaviors.hpp
+++ spot_behaviors/include/spot_behaviors/spot_behaviors.hpp
@@ -41,7 +41,6 @@
 #include "spot_behaviors/dock_robot.hpp"
 #include "spot_behaviors/move_hand_through_poses.hpp"
 #include "spot_behaviors/move_hand_to_pose.hpp"
-// #include "spot_behaviors/navigate_to_pose.hpp" // Scheduled for deletion; moved to nrg_navigation_behaviors
 #include "spot_behaviors/walk_to_pose.hpp"
 
 namespace spot_behaviors {
@@ -59,7 +58,6 @@ void registerSpotBehaviors(BT::BehaviorTreeFactory& factory, tf2_ros::Buffer::Sh
     REGSITER_SPOT_BEHAVIOR(DockRobot);
     REGSITER_SPOT_BEHAVIOR(MoveHandThroughPoses);
     REGSITER_SPOT_BEHAVIOR(MoveHandToPose);
-    // REGSITER_SPOT_BEHAVIOR(NavigateToPose);
     REGSITER_SPOT_BEHAVIOR(WalkToPose);
 
     // A manifest of subtrees and their requirements
