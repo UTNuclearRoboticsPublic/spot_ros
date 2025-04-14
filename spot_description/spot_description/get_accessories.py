689c0bc71604491a16e6144591135bbff1392195
alexnavtt <alexnavtt@utexas.edu>
2025-04-14 08:24:10 -0600

    Cleaned up accessory detection function to be easier to scale in the future

--- spot_description/spot_description/get_accessories.py
+++ spot_description/spot_description/get_accessories.py
@@ -1,20 +1,19 @@
 import os
 
+# All currently supported payloads for Spot
+accessory_to_arg_map = {
+    'ARM'   : 'has_arm',
+    'EAP'   : 'has_eap',
+    'EAP2'  : 'has_eap_2',
+    'RL_KIT': 'has_rl_kit', 
+}
+
 def get_accessories_from_env():
     spot_accessories: str = os.getenv('SPOT_ACCESSORIES', '')
     spot_accessories_dict: dict = {}
     for accessory in spot_accessories.split(' '):
-        if accessory == 'ARM':
-            spot_accessories_dict['has_arm'] = 'True'
-        elif accessory == 'EAP':
-            spot_accessories_dict['has_eap'] = 'True'
-        elif accessory == 'EAP2':
-            spot_accessories_dict['has_eap_2'] = 'True'
-        elif accessory == 'CAM':
-            spot_accessories_dict['has_cam_payload'] = 'True'
-        elif accessory == 'RL_KIT':
-            spot_accessories_dict['has_rl_kit'] = 'True'
-        elif accessory == 'REALSENSE':
-            spot_accessories_dict['has_realsense'] = 'True'
+        if accessory in accessory_to_arg_map:
+            arg_name = accessory_to_arg_map[accessory]
+            spot_accessories_dict[arg_name] = 'True'
 
     return spot_accessories_dict
