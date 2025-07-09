634cbd2006ceb6898b3ca15919d7bcc3cae8b439
cjans121 <jpanthi@utexas.edu>
2025-07-09 11:00:07 -0600

    implement timed lookup inside get_ros_image

--- spot_utils/src/spot_utils/camera_client.cpp
+++ spot_utils/src/spot_utils/camera_client.cpp
@@ -11,7 +11,29 @@ CameraClient::CameraClient(const rclcpp::Node::SharedPtr &node,
 }
 
 // Takes a snapshot of the current image and camera information
-sensor_msgs::msg::Image CameraClient::get_ros_image() { return image_; }
+sensor_msgs::msg::Image CameraClient::get_ros_image() {
+  using namespace std::chrono_literals;
+
+  std::chrono::seconds snapshot_timeout_duration(
+      image_lookup_timeout_secs_); // Configurable timeout
+
+  auto start_time = std::chrono::steady_clock::now();
+
+  while (image_.encoding.empty()) {
+    rclcpp::spin_some(node_);
+    loop_rate.sleep();
+
+    // Check timeout
+    auto current_time = std::chrono::steady_clock::now();
+    if (current_time - start_time > snapshot_timeout_duration) {
+      throw BT::RuntimeError(
+          "Failed to get images within allotted timeout for camera: " +
+          this->camera_ns);
+    }
+  }
+
+  return transform_image_(image_);
+}
 
 std::string CameraClient::get_base64_image(
     const std::vector<std::shared_ptr<CameraClient>> &camera_client_list,
@@ -41,9 +63,9 @@ std::string CameraClient::get_base64_image(
     }
   }
 
-  auto transformed_img = camera_client->transform_image(img);
+  auto transformed_img = camera_client->transform_image_(img);
 
-  const std::string base64_img = convert_msg_to_base64(transformed_img);
+  const std::string base64_img = convert_msg_to_base64_(transformed_img);
 
   return base64_img;
 }
@@ -55,7 +77,7 @@ void CameraClient::destroy_subscription() {
 
 // Rotates an image to be erect
 sensor_msgs::msg::Image
-CameraClient::transform_image(const sensor_msgs::msg::Image &ros_image) {
+CameraClient::transform_image_(const sensor_msgs::msg::Image &ros_image) {
 
   // Create a shared_ptr msg from the image
   const auto msg = std::make_shared<sensor_msgs::msg::Image>(ros_image);
@@ -119,7 +141,7 @@ initialize_camera_clients_(const std::shared_ptr<rclcpp::Node> &node,
   camera_client_list.reserve(camera_list.size());
   try {
     for (const auto &camera_name : camera_list) {
-      CamInfo cam_info = this->get_camera_info_(camera_name);
+      CamInfo cam_info = this->get_camera_info(camera_name);
       camera_client_list.emplace_back(
           std::make_shared<CameraClient>(node, cam_info));
     }
@@ -132,7 +154,7 @@ initialize_camera_clients_(const std::shared_ptr<rclcpp::Node> &node,
   return true;
 }
 
-CamInfo get_camera_info_(const CameraName &camera_name) {
+CamInfo get_camera_info(const CameraName &camera_name) {
   CamInfo camera_info;
   camera_info.camera_name = camera_name;
 
@@ -167,8 +189,8 @@ CamInfo get_camera_info_(const CameraName &camera_name) {
 
 // Convert cv::Mat to a base64-encoded string with a specified format
 std::string
-CameraClient::convert_mat_to_base64(const cv::Mat &input,
-                                    const std::string &imageFormat) {
+CameraClient::convert_mat_to_base64_(const cv::Mat &input,
+                                     const std::string &imageFormat) {
   // Encode the cv::Mat to a specified image format (e.g., JPEG, PNG)
   std::vector<unsigned char> buffer;
   std::vector<int> params;
@@ -197,7 +219,7 @@ CameraClient::convert_mat_to_base64(const cv::Mat &input,
   return encodedImage;
 }
 std::string
-CameraClient::convert_msg_to_base64(const sensor_msgs::msg::Image &ros_image) {
+CameraClient::convert_msg_to_base64_(const sensor_msgs::msg::Image &ros_image) {
 
   try {
     // Create a shared_ptr msg from the image
@@ -210,21 +232,7 @@ CameraClient::convert_msg_to_base64(const sensor_msgs::msg::Image &ros_image) {
     // Get OpenCV Mat from cv_bridge
     const cv::Mat image = cv_ptr->image;
 
-    // // Save the image to a file
-    // if (!cv::imwrite("transformed_result.jpg", image))
-    // {
-    //     throw std::runtime_error("Failed to write image to file ");
-    // }
-
-    // auto serialized_mat = serializeMatToStringWithFormat(image, "jpeg");
-
-    // Encode the image to base64
-    // std::vector<uchar> buf;
-    // cv::imencode(".jpeg", image, buf);
-    // std::string base64_str = base64::to_base64(reinterpret_cast<const char
-    // *>(buf.data())); std::string base64_str =
-    // base64::to_base64(serialized_mat); return base64_str;
-    return convert_mat_to_base64(image, "jpeg");
+    return convert_mat_to_base64_(image, "jpeg");
   } catch (cv_bridge::Exception &e) {
     throw BT::RuntimeError(std::string("Error during image conversion: ") +
                            e.what());
