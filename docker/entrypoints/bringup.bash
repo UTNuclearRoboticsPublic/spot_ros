c0f5b94159c7dda22a228f6c80bafea7da2c631b
Steven Swanbeck <steven.swanbeck@gmail.com>
2025-10-10 14:32:28 -0500

    added manipulation as submodule

--- docker/entrypoints/bringup.bash
+++ docker/entrypoints/bringup.bash
@@ -2,4 +2,4 @@
 
 source /root/.bashrc
 source /colcon_ws/install/setup.bash
-ros2 launch spot_bringup bringup.launch.py hostname:=192.168.50.3 "$@"
+ros2 launch spot_bringup bringup.launch.py hostname:=192.168.50.3 dock_id:="${DOCK_ID:-520}" launch_velodyne:="${LAUNCH_VELODYNE:-true}" "$@"
