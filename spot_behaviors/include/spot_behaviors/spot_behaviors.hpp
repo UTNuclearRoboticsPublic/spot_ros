064daa68186d2f7e912b1867b4c67cb2be637fd8
alexnavtt <alexnavtt@utexas.edu>
2025-10-17 12:03:33 -0600

    Added missing include file

--- spot_behaviors/include/spot_behaviors/spot_behaviors.hpp
+++ spot_behaviors/include/spot_behaviors/spot_behaviors.hpp
@@ -42,6 +42,7 @@
 #include "spot_behaviors/move_hand_through_poses.hpp"
 #include "spot_behaviors/move_hand_to_pose.hpp"
 #include "spot_behaviors/walk_to_pose.hpp"
+#include "spot_behaviors/get_stow_state.hpp"
 #include "spot_behaviors/get_spot_ik.hpp"
 #include "spot_behaviors/toggle_payload.hpp"
 #include "spot_behaviors/get_images.hpp"
@@ -65,7 +66,7 @@ void registerSpotBehaviors(BT::BehaviorTreeFactory& factory, tf2_ros::Buffer::Sh
     REGSITER_SPOT_BEHAVIOR(CheckHandCollision);
     REGSITER_SPOT_BEHAVIOR(DockRobot);
     REGSITER_SPOT_BEHAVIOR(MoveHandThroughPoses);
-    REGSITER_SPOT_BEHAVIOR(MoveHandToPose);
+    // REGSITER_SPOT_BEHAVIOR(MoveHandToPose);
     REGSITER_SPOT_BEHAVIOR(WalkToPose);
     REGSITER_SPOT_BEHAVIOR(GetSpotIK);
     REGSITER_SPOT_BEHAVIOR(TogglePayload);
