9c077d517d55c516d8be599b69834c248f2f661e
alexnavtt <alexnavtt@utexas.edu>
2026-01-20 13:20:10 -0700

    Implemented simulated depth camera

--- spot_simulation/spot_simulation/simulated_depth_camera.py
+++ spot_simulation/spot_simulation/simulated_depth_camera.py
@@ -2,23 +2,38 @@ import open3d
 import numpy as np
 from math import pi
 from rclpy.logging import get_logger
+from sensor_msgs.msg import CameraInfo
+from scipy.spatial.transform import Rotation
 
 class SimulatedDepthCamera:
-    def __init__(self, depth_config):
-        self.config = depth_config
+    def __init__(self, sensor_config):
+        self.sensor_config = sensor_config
+        self.depth_config = sensor_config.depth_config
         self.pose = open3d.core.Tensor(np.eye(4), dtype=open3d.core.Dtype.Float32)
 
         self.intrinsic_matrix = open3d.core.Tensor(
-            np.array([[depth_config.fx, 0.0, depth_config.cx],
-                      [0.0, depth_config.fy, depth_config.cy],
+            np.array([[self.depth_config.fx, 0.0, self.depth_config.cx],
+                      [0.0, self.depth_config.fy, self.depth_config.cy],
                       [0.0,        0.0     ,        1.0     ]]))
         
-        # TODO: Store CameraInfo for easy access
+        self.camera_info = CameraInfo()
+        self.camera_info.k = self.intrinsic_matrix.numpy().astype(float).flatten().tolist()
+        self.camera_info.p = np.hstack((self.intrinsic_matrix.numpy(), np.zeros((3, 1), dtype=float))).astype(float).flatten().tolist()
+        self.camera_info.r = np.eye(3).flatten().tolist()
+        self.camera_info.distortion_model = 'plumb_bob'
+        self.camera_info.height = self.depth_config.vertical_resolution
+        self.camera_info.width = self.depth_config.horizontal_resolution
+        self.camera_info.header.frame_id = sensor_config.frame_id
 
     def generate_rays(self, scene):
-        return scene.create_rays_pinhole(
+        extrinsic = open3d.core.Tensor(np.eye(4), dtype=open3d.core.Dtype.Float32)
+        extrinsic[:3, :3] = self.pose[:3, :3].T()
+        extrinsic[:3,  3] = (-extrinsic[:3, :3] @ self.pose[:3, 3]).reshape((3,))
+        
+        rays = scene.create_rays_pinhole(
             self.intrinsic_matrix,
-            self.pose,
-            self.config.horizontal_resolution,
-            self.config.vertical_resolution
+            extrinsic,
+            self.depth_config.horizontal_resolution,
+            self.depth_config.vertical_resolution
         )
+        return rays
