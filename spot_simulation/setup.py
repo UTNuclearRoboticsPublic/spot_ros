299d1383117da44192e08c01c3df3a1fc2a4d05a
alexnavtt <alexnavtt@utexas.edu>
2026-01-19 22:12:02 -0700

    Created launch file and rviz config

--- spot_simulation/setup.py
+++ spot_simulation/setup.py
@@ -18,7 +18,8 @@ setup(
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*launch*')),
-        (os.path.join('share', package_name, 'config'), glob('config/*.yaml'))
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
