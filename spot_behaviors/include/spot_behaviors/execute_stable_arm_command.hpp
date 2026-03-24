be26472ccb625568dee600134ae11a576d5cf294
alexnavtt <alexnavtt@utexas.edu>
2026-03-24 09:58:38 -0600

    Added a behavior for calling the pre-known stable arm command action service

---
+++ spot_behaviors/include/spot_behaviors/execute_stable_arm_command.hpp
@@ -0,0 +1,100 @@
+////////////////////////////////////////////////////////////////////////////////////////////
+//      Title     : execute_stable_arm_command.hpp
+//      Project   : spot_ros
+//      Copyright : Copyright© The University of Texas at Austin, 2024. All rights reserved.
+//                
+//          All files within this directory are subject to the following, unless an alternative
+//          license is explicitly included within the text of each file.
+//
+//          This software and documentation constitute an unpublished work
+//          and contain valuable trade secrets and proprietary information
+//          belonging to the University. None of the foregoing material may be
+//          copied or duplicated or disclosed without the express, written
+//          permission of the University. THE UNIVERSITY EXPRESSLY DISCLAIMS ANY
+//          AND ALL WARRANTIES CONCERNING THIS SOFTWARE AND DOCUMENTATION,
+//          INCLUDING ANY WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+//          PARTICULAR PURPOSE, AND WARRANTIES OF PERFORMANCE, AND ANY WARRANTY
+//          THAT MIGHT OTHERWISE ARISE FROM COURSE OF DEALING OR USAGE OF TRADE.
+//          NO WARRANTY IS EITHER EXPRESS OR IMPLIED WITH RESPECT TO THE USE OF
+//          THE SOFTWARE OR DOCUMENTATION. Under no circumstances shall the
+//          University be liable for incidental, special, indirect, direct or
+//          consequential damages or loss of profits, interruption of business,
+//          or related expenses which may arise from use of software or documentation,
+//          including but not limited to those resulting from defects in software
+//          and/or documentation, or loss or inaccuracy of data of any kind.
+//
+////////////////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <optional>
+#include <rclcpp/rclcpp.hpp>
+#include <behaviortree_cpp/action_node.h>
+#include <rclcpp_action/rclcpp_action.hpp>
+#include "spot_behaviors/node_behavior_base.hpp"
+#include "spot_msgs/action/stable_arm_command.hpp"
+
+namespace spot_behaviors{
+
+/**
+ * === ExecuteStableArmCommand ===
+
+ * This behavior bypasses the motion planning step for arm motions using the
+ * stable arm motion server by assuming that you already have a traejctory and
+ * simply wish to execute it. This can happen when motion planning is performed 
+ * by a dedicated node ahead of time, and when the state of the robot during
+ * planning is not the current state of the robot. In that case, simply calling
+ * the ExecuteTrajectory service of the stable motion server would calculate the 
+ * end-effector poses relative to the current body pose, which would lead to minor
+ * inaccuracies in position if the robot is currntly leaning due to a deployed arm.
+ */
+
+class ExecuteStableArmCommand : public BT::StatefulActionNode, public NodeBehaviorBase {
+public:
+    ExecuteStableArmCommand(const std::string& name, const BT::NodeConfiguration& config, tf2_ros::Buffer::SharedPtr tf_buffer);
+
+    /** We accept x input ports - TODO 
+     *  - target_pose: geometry_msgs::msg::PoseStamped 
+     */
+    static BT::PortsList providedPorts();
+
+    /** 
+     * Make the the motion request. 
+     * @return RUNNING if the action server is available, FAILURE otherwise
+     */
+    BT::NodeStatus onStart() override;
+
+    /**
+     * Check the status of an existing request
+     * @return RUNNING if request is not yet complete, FAILURE if no requset has
+     *         been made, SUCCESS/FAILURE if the request was completed  
+     */
+    BT::NodeStatus onRunning() override;
+
+    /**
+     * If an there is an existing request, cancel it 
+     */
+    void onHalted() override;
+
+protected:
+
+    // Action client
+    rclcpp_action::Client<spot_msgs::action::StableArmCommand>::SharedPtr motion_action_client_;
+
+    // Action client future handle - only used while waiting for a request to be accepted or jejected
+    std::optional<std::shared_future<rclcpp_action::ClientGoalHandle<spot_msgs::action::StableArmCommand>::SharedPtr>> motion_response_future_;
+    rclcpp::Time request_timestamp_{};
+    bool hasOngoingMotionRequest() const {return motion_response_future_.has_value();};
+    BT::NodeStatus checkMotionRequestStatus();
+
+    // Action client goal handle - nullptr if no request is active
+    rclcpp_action::ClientGoalHandle<spot_msgs::action::StableArmCommand>::SharedPtr motion_goal_handle_;
+    rclcpp::Time motion_start_time_;
+    bool hasOngoingMotionExecution() const {return static_cast<bool>(motion_goal_handle_);}
+    BT::NodeStatus checkMotionExecutionStatus();
+
+    template<typename ActionType>
+    BT::NodeStatus checkRequestStatus(std::shared_future<typename rclcpp_action::ClientGoalHandle<ActionType>::SharedPtr> shared_future);
+};
+
+} // namespace spot_behaviors
---
+++ spot_behaviors/src/execute_stable_arm_command.cpp
@@ -0,0 +1,153 @@
+#include <geometry_msgs/msg/pose_array.hpp>
+#include <moveit/kinematic_constraints/utils.h>
+#include "spot_behaviors/execute_stable_arm_command.hpp"
+
+namespace spot_behaviors{
+
+ExecuteStableArmCommand::ExecuteStableArmCommand(const std::string& name, const BT::NodeConfiguration& config, tf2_ros::Buffer::SharedPtr tf_buffer):
+    BT::StatefulActionNode(name, config),
+    NodeBehaviorBase(name, tf_buffer)
+{
+    motion_action_client_ = rclcpp_action::create_client<spot_msgs::action::StableArmCommand>(this, "/spot_moveit/execute_known_stable_trajectory");
+}
+
+// ------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
+
+BT::PortsList ExecuteStableArmCommand::providedPorts() {
+    return {
+        BT::InputPort<trajectory_msgs::msg::JointTrajectory::SharedPtr>("joint_trajectory", "The sequence of joint states through which to move the arm"),
+        BT::InputPort<geometry_msgs::msg::PoseArray::SharedPtr>("end_effector_trajectory", "The sequence of end effector poses through which to move the arm0_hand frame"),
+    };
+}
+    
+// ------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
+
+BT::NodeStatus ExecuteStableArmCommand::onStart() {
+    if (!motion_action_client_->wait_for_action_server(std::chrono::seconds(1))) {
+        RCLCPP_ERROR(get_logger(), "Unable to connect to action server, aborting ExecuteStableArmCommand");
+        return BT::NodeStatus::FAILURE;
+    }
+    
+    trajectory_msgs::msg::JointTrajectory::SharedPtr joint_trajectory;
+    if (!getInput("joint_trajectory", joint_trajectory) || !joint_trajectory) {
+        RCLCPP_ERROR(get_logger(), "Unable to read joint trajectory from blackboard, aborting ExecuteStableArmCommand");
+        return BT::NodeStatus::FAILURE;
+    }
+
+    geometry_msgs::msg::PoseArray::SharedPtr end_effector_trajectory;
+    if (!getInput("end_effector_trajectory", end_effector_trajectory) || !end_effector_trajectory) {
+        RCLCPP_ERROR(get_logger(), "Unable to read end effector trajectory from blackboard, aborting ExecuteStableArmCommand");
+        return BT::NodeStatus::FAILURE;
+    }
+
+    spot_msgs::action::StableArmCommand::Goal goal;
+    goal.joint_trajectory = *joint_trajectory;
+    goal.pose_waypoints = *end_effector_trajectory;
+
+    motion_response_future_ = motion_action_client_->async_send_goal(goal);
+    request_timestamp_ = now();
+
+    return BT::NodeStatus::RUNNING;
+}
+
+// ------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
+
+BT::NodeStatus ExecuteStableArmCommand::onRunning() {
+    if (hasOngoingMotionRequest()) {
+        return checkMotionRequestStatus();
+    }
+
+    else if (hasOngoingMotionExecution()) {
+        return checkMotionExecutionStatus();
+    }
+
+    else {
+        return BT::NodeStatus::FAILURE;
+    }
+}
+
+// ------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
+
+void ExecuteStableArmCommand::onHalted() {
+    if (hasOngoingMotionRequest() || hasOngoingMotionExecution()) {
+        motion_action_client_->async_cancel_all_goals();
+        motion_goal_handle_.reset();
+        motion_response_future_.reset();
+    }
+}
+
+// ------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
+
+BT::NodeStatus ExecuteStableArmCommand::checkMotionRequestStatus() {
+    auto status = rclcpp::spin_until_future_complete(this->get_node_base_interface(), motion_response_future_.value(), std::chrono::milliseconds(5));
+    switch (status) {
+        case rclcpp::FutureReturnCode::TIMEOUT: {
+            const auto max_duration = std::chrono::seconds(5);
+            if (now() - request_timestamp_ > max_duration) {
+                RCLCPP_ERROR(get_logger(), "Did not get a response from the server within the time limit, aborting ExecuteStableArmCommand");
+                halt();
+                return BT::NodeStatus::FAILURE;
+            }
+            return BT::NodeStatus::RUNNING;
+        }
+    
+        default:
+        case rclcpp::FutureReturnCode::INTERRUPTED: {
+            RCLCPP_WARN(get_logger(), "ExecuteStableArmCommand path generation step interrupted, returning failure");
+            halt();
+            return BT::NodeStatus::FAILURE;
+        }
+
+        case rclcpp::FutureReturnCode::SUCCESS: {
+            motion_goal_handle_ = motion_response_future_->get();
+            motion_response_future_.reset();
+            if (!motion_goal_handle_) {
+                RCLCPP_WARN(get_logger(), "Plan rejected by server");
+                return BT::NodeStatus::FAILURE;
+            }
+            return BT::NodeStatus::RUNNING;
+        }
+    }
+}
+
+// ------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
+
+BT::NodeStatus ExecuteStableArmCommand::checkMotionExecutionStatus() {
+    // Possibility two - goal is active and we check its status
+    rclcpp::spin_some(this->get_node_base_interface());
+    const int8_t goal_status = motion_goal_handle_->get_status();
+    switch (goal_status){
+        case action_msgs::msg::GoalStatus::STATUS_CANCELING:
+        case action_msgs::msg::GoalStatus::STATUS_ACCEPTED:
+        case action_msgs::msg::GoalStatus::STATUS_EXECUTING:
+            return BT::NodeStatus::RUNNING;
+
+        case action_msgs::msg::GoalStatus::STATUS_UNKNOWN:
+            RCLCPP_WARN(get_logger(), "server returned status UNKNOWN, reporting failure");
+            [[fallthrough]];
+        case action_msgs::msg::GoalStatus::STATUS_ABORTED:
+        case action_msgs::msg::GoalStatus::STATUS_CANCELED:
+            RCLCPP_WARN(get_logger(), "Motion execution failed");
+            motion_goal_handle_.reset();
+            return BT::NodeStatus::FAILURE;
+            
+        case action_msgs::msg::GoalStatus::STATUS_SUCCEEDED:
+            RCLCPP_INFO(get_logger(), "Motion execution completed successfully");
+            motion_goal_handle_.reset();
+            return BT::NodeStatus::SUCCESS;
+        }
+
+    RCLCPP_ERROR(get_logger(), "Server returned unknown status code \"%d\", reporting failure", +goal_status);
+    return BT::NodeStatus::FAILURE;
+}
+
+// ------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
+
+} // namespace spot_behaviors
