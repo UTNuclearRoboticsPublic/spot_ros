f596580f79cff06f57272883833227d2a9673ee3
alexnavtt <alexnavtt@utexas.edu>
2026-04-10 17:35:59 -0600

    Added comments simulated RGB camera

--- spot_simulation/spot_simulation/simulated_rgb_camera.py
+++ spot_simulation/spot_simulation/simulated_rgb_camera.py
@@ -10,7 +10,6 @@ from .simulated_object import SimulatedObject
 from rclpy.logging import get_logger
 
 class SimulatedRGBCamera:
-    render_scene = None
     render_thread: multiprocessing.Process = None
     max_width = 0
     max_height = 0
@@ -18,19 +17,26 @@ class SimulatedRGBCamera:
     render_requests = multiprocessing.Queue()
     render_completion_events = {}
     render_buffers = {}
+    started = False
 
     def __init__(self, sensor_config):
+        if SimulatedRGBCamera.started and ((sensor_config.rgb_config.vertical_resolution > SimulatedRGBCamera.max_height) or (sensor_config.rgb_config.horizontal_resolution > SimulatedRGBCamera.max_width)):
+            raise RuntimeError('Cannot adjust the rendering resolution after the render thread has been started! Add all cameras before starting the thread')
+
+        # Register this camera in the global list
         SimulatedRGBCamera.render_buffers[id(self)] = None
+        
+        # Record camera config
         self.sensor_config = sensor_config
         self.rgb_config = sensor_config.rgb_config
         self.pose = np.eye(4, dtype=np.float64)
-
         self.intrinsic_matrix = np.array(
             [[self.rgb_config.fx, 0.0, self.rgb_config.cx],
              [0.0, self.rgb_config.fy, self.rgb_config.cy],
              [0.0,        0.0        ,        1.0        ]],
         dtype=np.float64)
         
+        # Set up CameraInfo message. It will be the same for all images except for the timestamp
         self.camera_info = CameraInfo()
         self.camera_info.d = [0.0, 0.0, 0.0, 0.0, 0.0]
         self.camera_info.k = self.intrinsic_matrix.astype(float).flatten().tolist()
@@ -41,6 +47,7 @@ class SimulatedRGBCamera:
         self.camera_info.width = self.rgb_config.horizontal_resolution
         self.camera_info.header.frame_id = sensor_config.frame_id
 
+        # Keep track of max resolution. Render thread runs at max resolution and resizes down any images that are smaller
         SimulatedRGBCamera.max_height = max(SimulatedRGBCamera.max_height, self.rgb_config.vertical_resolution)
         SimulatedRGBCamera.max_width = max(SimulatedRGBCamera.max_width, self.rgb_config.horizontal_resolution)
 
@@ -50,21 +57,26 @@ class SimulatedRGBCamera:
         colors = [np.array([object.marker.color.r, object.marker.color.g, object.marker.color.b]) for object in geometries.values()]
         vertices = [np.asarray(object.rendering_geometry.vertices) for object in geometries.values()]
         triangles = [np.asarray(object.rendering_geometry.triangles) for object in geometries.values()]
+
+        # Register buffers and multiprocessing events for each object
         for key in SimulatedRGBCamera.render_buffers.keys():
             SimulatedRGBCamera.render_buffers[key] = multiprocessing.Array('B', SimulatedRGBCamera.max_width * SimulatedRGBCamera.max_height * 3)
             SimulatedRGBCamera.render_completion_events[key] = multiprocessing.Event()
 
+        # Start the rendering thread in a new process. The offscreen renderer only works if called by the main thread
+        # TODO: Evaluate memory vs. runtime cost of creating a new process for each camera
         SimulatedRGBCamera.render_thread = multiprocessing.Process(
             target=SimulatedRGBCamera.renderingThread,
             args=(names, colors, vertices, triangles)
         )
         SimulatedRGBCamera.render_thread.start()
+        SimulatedRGBCamera.started = True
 
     def renderingThread(names: list[str], colors: list[list[float]], vertices: list[np.ndarray], triangles: list[np.ndarray]):
         get_logger('SimulatedRGBCamera').info(f'Creating an OffscreenRenderer with image size {SimulatedRGBCamera.max_width}x{SimulatedRGBCamera.max_height}')
-        SimulatedRGBCamera.render_scene = open3d.visualization.rendering.OffscreenRenderer(SimulatedRGBCamera.max_width, SimulatedRGBCamera.max_height)
+        render_scene = open3d.visualization.rendering.OffscreenRenderer(SimulatedRGBCamera.max_width, SimulatedRGBCamera.max_height)
 
-
+        # Reconstruct the geometry and add it to the render scene
         for name, color, vertex_set, triangle_set in zip(names, colors, vertices, triangles):
             geometry = open3d.geometry.TriangleMesh(
                 vertices=open3d.utility.Vector3dVector(vertex_set),
@@ -74,7 +86,7 @@ class SimulatedRGBCamera:
             material = open3d.visualization.rendering.MaterialRecord()
             material.base_color = [*color, 1.0]
             material.shader = "defaultLit"
-            SimulatedRGBCamera.render_scene.scene.add_geometry(name, geometry, material)
+            render_scene.scene.add_geometry(name, geometry, material)
 
         while rclpy.ok():
             # Wait for a new request
@@ -83,14 +95,18 @@ class SimulatedRGBCamera:
             rendered_image = np.frombuffer(SimulatedRGBCamera.render_buffers[obj_id].get_obj(), dtype=np.uint8).reshape(SimulatedRGBCamera.max_height, SimulatedRGBCamera.max_width, 3)
 
             # Render the image
-            SimulatedRGBCamera.render_scene.setup_camera(intrinsic_matrix, extrinsic_matrix, image_width_px, image_height_px)
-            open3d_image = SimulatedRGBCamera.render_scene.render_to_image()
+            render_scene.setup_camera(intrinsic_matrix, extrinsic_matrix, image_width_px, image_height_px)
+            open3d_image = render_scene.render_to_image()
 
             # Place into the response queue
             np.copyto(rendered_image, np.asarray(open3d_image))
             SimulatedRGBCamera.render_completion_events[obj_id].set()
 
     def getImage(self) -> Image:
+        if not SimulatedRGBCamera.started:
+            raise RuntimeError('Cannot generate image: rendering thread has not been started. Call SimulatedRGBCamera.start!')
+
+        # Submit a render request and wait for it to complete
         extrinsic = np.linalg.inv(self.pose)
         SimulatedRGBCamera.render_requests.put((
             self.intrinsic_matrix,
@@ -101,6 +117,8 @@ class SimulatedRGBCamera:
         ))
         SimulatedRGBCamera.render_completion_events[id(self)].wait()
         SimulatedRGBCamera.render_completion_events[id(self)].clear()
+
+        # After completion, copy to a numpy buffer and set to the image shape
         open3d_image = np.copy(SimulatedRGBCamera.render_buffers[id(self)].get_obj()).reshape(SimulatedRGBCamera.max_height, SimulatedRGBCamera.max_width, 3)
 
         # Resize and convert to ROS
